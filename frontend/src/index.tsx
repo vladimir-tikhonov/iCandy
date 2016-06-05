f577ab9e874ef12699ecaba986868b721609bf98
Vladimir Tikhonov <vladimir@tikhonov.by>
2016-06-05 13:29:41 +0300

    Add redux & react-redux

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,19 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import Routes from "./scripts/components/Routes";
+import reducer from "./scripts/reducers/reducer";
 
 import "style!css!react-virtualized/styles.css";
 
-ReactDom.render(<Routes />, document.getElementById("main"));
+const store = createStore(reducer);
+
+const Root = () => (
+    <Provider store={store}>
+        <Routes />
+    </Provider>
+);
+
+ReactDom.render(<Root />, document.getElementById("main"));
---
+++ frontend/src/scripts/reducers/session.ts
@@ -0,0 +1,5 @@
+function session(state = {}, _action) {
+  return state;
+}
+
+export default session;
